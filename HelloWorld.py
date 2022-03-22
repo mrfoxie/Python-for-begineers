2232668a280048ea3395713b85c7286869c5a2d3
mrfoxie <siddh.mistry396321@gmail.com>
2022-03-22 20:14:41 +0530

    Hello World Updated

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,7 +2,7 @@
 # print also us helps to display message of the task which is being done when the program is being executed
 # In python programming are been scanned from first line of the
 print('Hello World')  # to print any kind of messages to user of inform user
-# Drawing an imaginary dog
-print('\ /    /')
-print(' o----/')
-print('  ||||')
+# Try yourself by using print command down bellow
+print('Your message!') # write something in between apostrophe or you can use quotes to display your message
+# Or
+print("Your Message!")
