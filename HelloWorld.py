66748da627a333ba4fb5d85983ea90f298b92e5f
Mr. Blogger <siddh.mistry396321@gmail.com>
2022-03-23 16:06:43 +0530

    README added

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,8 +1,17 @@
-# 'print' is used to print any text or numerical data which is witten by user.
-# print also us helps to display message of the task which is being done when the program is being executed
-# In python programming are been scanned from first line of the
+# 'print' is used to print any text or numerical data which is witten by user. print also us helps to display message
+# of the task which is being done when the program is being executed In python programming are being scanned from
+# first line of the Now when you want to print something then you need quotations mark to print any-kind of string(
+# means display characters) to display out message which you want to print.
+
 print('Hello World')  # to print any kind of messages to user of inform user
-# Try yourself by using print command down bellow
-print('Your message!') # write something in between apostrophe or you can use quotes to display your message
-# Or
-print("Your Message!")
+
+# Try to draw an imaginary dog using print command.
+print('')
+
+print('* ' * 3)
+# hold on as I told you that we will be printing characters in quotation marks then why am I using
+# multiplication here well you will see the magic when you run it when you multiply sting with any number then it
+# will multiply the output and give you the answer in the above case it will print 3 time '*' like this *** << this
+# is your answer of the above line now you can try down bellow with your actions
+
+print('|' * 10)  # change 0 and characters in the quotation mark
