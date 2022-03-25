8de36ccdc2e441ca9b4173524c0bb7248406c72e
mrfoxie <siddh.mistry396321@gmail.com>
2022-03-25 10:07:47 +0530

    details added

--- Codes/01. HelloWorld.py
+++ Codes/01. HelloWorld.py
@@ -1,7 +1,7 @@
 # 'print' is used to print any text or numerical data which is witten by user. print also us helps to display message
 # of the task which is being done when the program is being executed In python programming are being scanned from
-# first line of the Now when you want to print something then you need quotations mark to print any-kind of string(
-# means display characters) to display out message which you want to print.
+# first line of the Now when you want to print something then you need quotations mark to print any-kind of string
+# (means display characters) to display out message which you want to print.
 
 print('Hello World')  # to print any kind of messages to user of inform user
 
@@ -18,3 +18,6 @@ print('* ' * 3)
 print('|' * 10)  # change 10 and characters in the quotation mark
 
 
+# Now what if I use addition instead of multiplication then what will happen. Let's give it a try.
+print('First word' + ' Second word')  # here both the words will add together and give you the answer 'First word Second word'
+# so when you use addition instead of multiplication will combine those words and give you a single word.
