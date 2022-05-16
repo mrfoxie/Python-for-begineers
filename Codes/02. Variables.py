61906b7471e59437645d84ea60897e440ed196e0
Mr. Blogger <siddh.mistry396321@gmail.com>
2022-05-16 12:15:55 +0530

    Variable is been updated

--- Codes/02. Variables.py
+++ Codes/02. Variables.py
@@ -3,8 +3,11 @@
 # Say for example if you want to save someone's age then you have to just use variable to save e.g. age = 25
 # After using the variable you can use it in the print statement to print age.
 
-age = 25
-print(age)  # you will get answer '25' in the terminal box
+age = 25  # just writing 25 means that it's your integer
+print(age)  # you will get answer '25' in the terminal box but if you add quote to age then it will print age
+
+degree = 15.5
+print(degree)  # you will get 15.5 which is your float number so as a result you will 15.5
 
 # You can use quotation marks to print string characters so that you can display.
 name = 'HelloWorld'
@@ -16,7 +19,22 @@ characters = 'Change_me'
 print(number)
 print(characters)
 
-# Now say for example I used 2 time the same variable with different variables. Lets check it out.
+# Now say for example I used 2 time the same variable with different variables. Let's check it out.
 python = 22
 python = 'I win.'
 print(python)  # run the program and try it by yourself and check the answer.
+
+# In some cases you need to define variable as a large name so for that you have to use underscore instead of spaces
+two_words = 'First Second'  # as you can see that I have writen 2 words as a variable but in between there is no
+# space I have use underscore so that it can understand as a single variable
+print(two_words)
+
+# Now we will be using boolean in python but remember one thing that python is a case-sensitive langauge so make sure
+# that you write in a proper formate
+is_true = True
+print(is_true)
+is_false = False
+print(is_false)
+# as you can see that I have use true or false as a boolean, but it's a case-sensitive, so you have to capitalize all
+# the characters which has been needed if not that it will prompt with you error you can try it by yourself by
+# changing True or False in above code
