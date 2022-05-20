7aa84cd1e5573361a52831820e01d699cf15e2d6
Mr. Blogger <siddh.mistry396321@gmail.com>
2022-05-20 15:25:47 +0530

    File changed and added

--- Codes/01. HelloWorld.py
+++ Codes/01. HelloWorld.py
@@ -11,14 +11,14 @@ print('')
 
 print('* ' * 3)
 # hold on as I told you that we will be printing characters in quotation marks then why am I using
-# multiplication here well you will see the magic when you run it when you multiply sting with any number then it
+# multiplication here well you will see the magic when you run it when you multiply sting with any number than it
 # will multiply the output and give you the answer in the above case it will print 3 time '*' like this *** << this
 # is your answer of the above line now you can try down bellow with your actions
 
 # Practice
 print('|' * 10)  # change 10 and characters in the quotation mark
 
-
 # Now what if I use addition instead of multiplication then what will happen. Let's give it a try.
-print('First word' + ' Second word')  # here both the words will add together and give you the answer 'First word Second word'
+print('First word' + ' Second word')
+# here both the words will add together and give you the answer 'First word Second word'
 # so when you use addition instead of multiplication will combine those words and give you a single word.
--- Codes/05. Example 02 Ans.py
+++ Codes/05. Example 02 Ans.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 name = input("Write your name: ")
-color = input("Enter your favourite color: ")
+color = input("Enter your favorite color: ")
 print('Your Answer is bellow.\n')
 print(name + ' likes ' + color + ' color.')
