d309a51c7a401e1eabe7a241a3619d1d506b0930
Mr. Blogger <siddh.mistry396321@gmail.com>
2022-05-30 12:33:25 +0530

    Example 04 added of characteristics.

---
+++ Codes/010. Example 04.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# now as you have learned characteristics to string then you must be having knowledge of starting points and ending points.
+# so what will be the answer of the following code.
+name = "Python"
+print(name[1:-1])
